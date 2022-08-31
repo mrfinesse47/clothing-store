99855187bfe900436bdb93f65083b44f903ed585
Kevin Mason <krmason2020@gmail.com>
2022-08-30 22:01:31 -0700

    styling added for navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import { Routes, Route } from 'react-router-dom';
+
 import Home from './routes/home/home.component';
+import Navigation from './routes/navigation/navigation.component';
 
 function App() {
+  const Shop = () => {
+    <div>
+      <h1>I am the shop page</h1>
+    </div>;
+  };
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path='shop' element={<Shop />} />
+      </Route>
     </Routes>
   );
 }
